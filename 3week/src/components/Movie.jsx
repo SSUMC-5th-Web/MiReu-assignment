728a60583c3b2adb322ca7263b163a56f1da9aea
iamyeongg <syz101@naver.com>
2023-10-11 02:18:52 +0900

    미션 질문

--- 3week/src/components/Movie.jsx
+++ 3week/src/components/Movie.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Overview from "./Overview.jsx";
 
 const ING_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
 
-export default function Movie({ title, poster_path, vote_average }) {
+export default function Movie({ title, poster_path, vote_average, overview }) {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsMouseOver(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMouseOver(false);
+  };
+
   return (
-    <div className="movie-container">
-      <img src={ING_BASE_URL+poster_path} alt="영화포스터" />
+    <div
+      className={`movie-container ${isMouseOver ? "show-overview" : ""}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img src={ING_BASE_URL + poster_path} alt="영화포스터" />
+
+      {isMouseOver && <Overview overview={overview} />}
       <div className="movie-info">
         <h3>{title}</h3>
         <span>{vote_average}</span>
@@ -13,3 +30,5 @@ export default function Movie({ title, poster_path, vote_average }) {
     </div>
   );
 }
+
+
---
+++ 3week/src/components/Overview.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Overview({overview}) {
+  return (
+      <div className="movie-info">
+        <h3>{overview}</h3>
+      </div>
+  );
+}
