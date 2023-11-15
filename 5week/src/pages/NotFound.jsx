c905acfd505382f2d6a498341871a292d175e277
iamyeongg <syz101@naver.com>
2023-11-15 15:43:35 +0900

    6week 과제 완료

--- 5week/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return (
-    <div className="page-contaniner" style={{ fontsize: "32px",color:"white" }}>
-      <h2>Not Found</h2>
-    </div>
-  );
-}
