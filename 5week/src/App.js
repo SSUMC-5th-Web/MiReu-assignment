cb6de140db132b503b460898050ccdb8708ed78d
iamyeongg <syz101@naver.com>
2023-11-16 14:17:45 +0900

    7week 과제 완료

--- 5week/src/App.js
+++ 5week/src/App.js
@@ -6,6 +6,7 @@ import Celebrity from "./pages/Celebrity";
 import Header from "./components/Header";
 import MovieDetail from "./pages/MovieDetail";
 import NotFound from "./pages/NotF";
+import Login from "./pages/login";  
 // 라우팅의 영향을 받지 않는 컴포넌트는 routes tag 밖에 위치해야 한다.
 // nav 바를 이용해 페이지 이동을 시킴
 // 활용 예제 - 영화 앱 만들기2
@@ -18,7 +19,8 @@ import NotFound from "./pages/NotF";
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Movies" element={<Movies />} />
-            <Route path="/movie/:title" element={<MovieDetail/>} />
+            <Route path="/movie/:title" element={<MovieDetail />} />
+            <Route path="/login" element={<Login/>}/>
             <Route path="/tv" element={<Tv />} />
             <Route path="/person" element={<Celebrity />} />
             <Route path="*" element={<NotFound />} />
